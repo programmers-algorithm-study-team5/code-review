ae863a8bead221b2131d9da3ec499349f713bd4d
김혜윤 <hhyeyun22@gmail.com>
2025-10-24 19:43:00 +0900

    Delete directory

--- 김혜윤/1주차/k번째수.py
+++
@@ -1,9 +0,0 @@
-def solution(array, commands):
-    answer = []
-    
-    for i, j, k in commands:
-        sub=array[i-1:j]
-        sub.sort()
-        answer.append(sub[k-1])
-        
-    return answer
